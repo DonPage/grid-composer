313eeeb401e999f249111490f1ae8790ce7f5b7a
Don Page <don.page.work@gmail.com>
2017-03-12 22:13:23 -0400

    added: another test for user config being passed into gridComposer.

--- test/composer.test.js
+++ test/composer.test.js
@@ -12,3 +12,17 @@ test('GridComp uses default config if no config is passed into constructor.', (t
   t.truthy(JSON.stringify(config) === JSON.stringify(defaultConfig));
 
 });
+
+test('GridComp uses user specified config if it\'s passed into the constructor', (t) => {
+  const nodes = ~~(Math.random() * 99) + 1;
+  const config = {
+    getsPublic: true,
+    maxNodes: nodes,
+    directory: '../'
+  };
+  const defaultConfig = new GridComposer().config;
+  const userConfig = new GridComposer(config).config;
+
+  t.falsy(JSON.stringify(userConfig) === JSON.stringify(defaultConfig));
+
+});
