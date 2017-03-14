33f31202f87f4ff0b8e51ece3a39c7249a10bbc6
Don Page <don.page.work@gmail.com>
2017-03-13 21:02:54 -0400

    started: initExpressRouter. not called yet.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-/**
- * Created by Don on 3/12/2017.
- */
+const express = require('express');
+const bodyParser = require('body-parser');
+const process = require('child_process');
+const exec = process.exec;
+const spawn = process.spawn;
+
 
 class GridComposer {
+  private router;
   constructor(config) {
     this.config = config || {};
 
@@ -10,6 +14,17 @@ class GridComposer {
     this.config.getsPublic = this.config.getsPublic || false;
     this.config.directory = this.config.directory || './';
     this.config.maxNodes = this.config.maxNodes || 20;
+  }
+
+  private initExpressRouter() {
+    const e = express();
+    e.use(bodyParser.urlencoded({extended: true}));
+    e.use(bodyParser.json());
+    this.router = express.Router();
+
+    this.router.get('/', (req, res) => {
+
+    })
   }
 }
 
